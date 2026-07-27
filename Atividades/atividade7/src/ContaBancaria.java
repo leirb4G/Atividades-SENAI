3de3f17d8193b8122533a05d53eacdfd8a375d13
leirb4G <gabriel.bandeira19991@gmail.com>
2026-07-27 19:09:31 -0300

    atividade

--- Atividades/atividade7/src/ContaBancaria.java
+++ Atividades/atividade7/src/ContaBancaria.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
 
 public class ContaBancaria {
+    private String titular;
+    private double saldo;
+    private int numeroConta;
+    private LocalDateTime agora = LocalDateTime.now();
+
+    public ContaBancaria(String titular) {
+        this.titular = titular;
+        this.saldo = 0.0;
+        this.numeroConta = agora.getNano();
+
+
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- Atividades/atividade7/src/Main.java
+++ Atividades/atividade7/src/Main.java
@@ -1,8 +1,10 @@
+
+
 public class Main {
     public static void main(String[] args) {
         ContaBancaria pessoa1 = new ContaBancaria("Joao");
 
-        System.out.println(get.titular);
+        System.out.println();
 
     }
 }
